71270529370940a2c2751cf2c732739ebbd581f4
Breno <breno.ara.cabral@gmail.com>
2024-09-08 11:29:25 -0300

    feat: add start page on localhost:9090

--- cmd/protocommtester/main.go
+++ cmd/protocommtester/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"net/http"
+	"log"
+)
+
 func main() {
-  panic("")
+	fs := http.FileServer(http.Dir("templates/"))
+  http.Handle("/", fs)
+	log.Println("Server started at localhost:9090")
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
